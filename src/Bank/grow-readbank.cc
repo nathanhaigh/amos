51feb06053fb18e3318c0e7d37930a612090f3af
adelcher <>
2003-06-03 20:39:39 +0000

    Allow fasta-like format reads to be input

--- src/Bank/grow-readbank.cc
+++ src/Bank/grow-readbank.cc
@@ -17,7 +17,12 @@
 using namespace std;
 
 
+const int  MAX_LINE = 1000;
 const int  NEW_SIZE = 1000;
+
+
+enum  Input_Format_t
+     {FASTA_FORMAT, CELERA_MSG_FORMAT};
 
 
 static string  Bank_Name;
@@ -26,6 +31,10 @@ static bool  Create_New_Bank = false;
   // If true, then start a new read bank
 static bool  Force_New_Bank = false;
   // If true, then force a new read bank by deleting existing one first
+static Input_Format_t  Input_Format = FASTA_FORMAT;
+  // Type of input
+static char  * Input_File_1, * Input_File_2;
+  // Names of input file(s)
 
 
 
@@ -48,7 +57,7 @@ int  main
 
    fprintf (stderr, "Starting on  %s  at  %s\n", __DATE__, __TIME__);
 
-   Verbose = 1;
+   Verbose = 0;
 
    Parse_Command_Line (argc, argv);
 
@@ -65,19 +74,67 @@ int  main
        read_bank . createStore (Bank_Name, "silly comment");
 
    ct = 0;
-   while  (msg . read (stdin))
-     if  (msg . getMsgType () == FRG_MSG)
-         {
-          string  s;
-          
-          ct ++;
-          read . setClearRange (msg . getClearRange ());
-          read . setComment (msg . getSource ());
-          read . setEID (msg . getAccession ());
-          read . setSequence (msg . getSequence (), msg . getQuality ());
-          iid = read_bank . append (read);
-         }
-   printf ("%d FRG messages\n", ct);
+   if  (Input_Format == CELERA_MSG_FORMAT)
+       {
+        FILE  * fp;
+        fp = File_Open (Input_File_1, "r");
+        while  (msg . read (fp))
+          if  (msg . getMsgType () == FRG_MSG)
+              {
+               string  s;
+
+               ct ++;
+               read . setClearRange (msg . getClearRange ());
+               read . setComment (msg . getSource ());
+               read . setEID (msg . getAccession ());
+               read . setSequence (msg . getSequence (), msg . getQuality ());
+               iid = read_bank . append (read);
+              }
+       }
+   else if  (Input_Format == FASTA_FORMAT)
+       {
+        FILE  * fp1, * fp2;
+        string  s, q, tag_line1, tag_line2;
+        char  id1 [MAX_LINE], id2 [MAX_LINE];
+
+        fp1 = File_Open (Input_File_1, "r");
+        fp2 = File_Open (Input_File_2, "r");
+
+        while  (Fasta_Read (fp1, s, tag_line1))
+          {
+           char  * p1, * p2;
+           int  len;
+
+           Fasta_Qual_Read (fp2, q, tag_line2);
+           strcpy (id1, tag_line1 . c_str ());
+           strcpy (id2, tag_line2 . c_str ());
+           p1 = strtok (id1, " \t\n");
+           p2 = strtok (id2, " \t\n");
+           if  (strcmp (p1, p2) != 0)
+               {
+                sprintf (Clean_Exit_Msg_Line, "seq id %s doesn't match quality id %s",
+                     p1, p2);
+                Clean_Exit (Clean_Exit_Msg_Line, __FILE__, __LINE__);
+               }
+           len = s . length ();
+           if  (s . length () != q . length ())
+               {
+                sprintf (Clean_Exit_Msg_Line,
+                     "seq length %d doesn't match quality length %d",
+                     len, int (q . length ()));
+                Clean_Exit (Clean_Exit_Msg_Line, __FILE__, __LINE__);
+               }
+
+           ct ++;
+           read . setClearRange (0, len);
+           read . setComment (p1);
+           sprintf (id1, "%d", ct);
+           read . setEID (id1);
+           read . setSequence (s . c_str (), q . c_str ());
+           iid = read_bank . append (read);
+          }
+       }
+   fprintf (stderr, "Processed %d reads\n", ct);
 
    read_bank . commitStore ();
    read_bank . closeStore ();
@@ -114,11 +171,15 @@ static void  Parse_Command_Line
 
    optarg = NULL;
 
-   while  (! errflg && ((ch = getopt (argc, argv, "cfh")) != EOF))
+   while  (! errflg && ((ch = getopt (argc, argv, "cCfh")) != EOF))
      switch  (ch)
        {
         case  'c' :
           Create_New_Bank = true;
+          break;
+
+        case  'C' :
+          Input_Format = CELERA_MSG_FORMAT;
           break;
 
         case  'f' :
@@ -142,12 +203,23 @@ static void  Parse_Command_Line
         exit (EXIT_FAILURE);
        }
 
-   if  (optind > argc - 1)
+   if  (Input_Format == CELERA_MSG_FORMAT && optind > argc - 2)
        {
-        fprintf (stderr, "ERROR:  bank name not specified\n");
+        fprintf (stderr, "ERROR:  Must specify bank name and .frg file\n");
+        Usage (argv [0]);
         exit (EXIT_FAILURE);
        }
+   if  (Input_Format == FASTA_FORMAT && optind > argc - 3)
+       {
+        fprintf (stderr, "ERROR:  Must specify bank name, seq  and qual files\n");
+        Usage (argv [0]);
+        exit (EXIT_FAILURE);
+       }
+
    Bank_Name = argv [optind ++];
+   Input_File_1 = argv [optind ++];
+   if  (Input_Format == FASTA_FORMAT)
+       Input_File_2 = argv [optind ++];
 
    return;
   }
@@ -163,13 +235,17 @@ static void  Usage
 
   {
    fprintf (stderr,
-           "USAGE:  %s  <bank-name>\n"
+           "USAGE:  %s  <bank-name> <input-file[s]>\n"
            "\n"
-           "Add reads (in Celera frg-file format) from stdin to the readbank\n"
-           "<bank-name>\n"
+           "Add reads from specified file(s) to the readbank\n"
+           "<bank-name>.  Default input format is fasta which\n"
+           "is two separate files (sequence and quality).\n"
+           "If Celera msg format is used, then only one file is\n"
+           "specified.\n"
            "\n"
            "Options:\n"
            "  -c    Create a new read bank\n"
+           "  -C    Input is Celera msg format, i.e., a .frg file\n"
            "  -f    Force new read bank by deleting existing one first\n"
            "  -h    Print this usage message\n"
            "\n",
