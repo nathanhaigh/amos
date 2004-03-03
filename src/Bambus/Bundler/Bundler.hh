d11fc797d931fa48ef91a2c4a8532784c99d9bf0
mpop <>
2004-03-03 19:20:16 +0000

    first version

---
+++ src/Bambus/Bundler/Bundler.hh
@@ -0,0 +1,17 @@
+// $Id$
+
+#ifndef BAMBUS_BUNDLER_HH
+#define BAMBUS_BUNDLER_HH
+
+#include <ContigLink_AMOS.hh>
+#include <ContigEdge_AMOS.hh>
+#include <string>
+#include <map>
+
+using namespace AMOS;
+
+typedef multimap<pair<ID_t, ID_t> , ContigLink_t > LinkMap_t;
+typedef map<ID_t, int > ContigLen_t;
+typedef map<string, ID_t> IdMap_t;
+
+#endif // BAMBUS_BUNDLER_HH
