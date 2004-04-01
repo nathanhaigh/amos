b84326a14fc8e6971d935f448e7b9cb9f355d46b
aphillip <>
2004-04-01 21:36:15 +0000

    added another for 0 based

--- src/Common/amp.hh
+++ src/Common/amp.hh
@@ -4,9 +4,15 @@
 #include <ctime>
 #include <cstring>
 
+// Return a character string representing the current date
 const char * Date ( );
 
-inline long long int RevC (long long int c, long long int len)
+// Reverse complement a 1 based coordinate
+inline long long int RevComp1 (long long int c, long long int len)
 { return len - c + 1; }
+
+// Reverse complement a 0 based coordinate
+inline long long int RevComp0 (long long int c, long long int len)
+{ return len - c - 1; }
 
 #endif // _AMP_HH
