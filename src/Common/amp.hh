2d185712b713bba00aded72df57c92053ccc059d
aphillip <>
2004-04-01 21:32:24 +0000

    added reverse complement for a coordinate

--- src/Common/amp.hh
+++ src/Common/amp.hh
@@ -4,7 +4,9 @@
 #include <ctime>
 #include <cstring>
 
-const char * Date
-     ( );
+const char * Date ( );
+
+inline long long int RevC (long long int c, long long int len)
+{ return len - c + 1; }
 
 #endif // _AMP_HH
