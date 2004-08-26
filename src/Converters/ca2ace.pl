e793ead3822f2f6054beaeb5415e7d5a6e0eb7ec
mpop <>
2004-08-26 15:22:15 +0000

    Removed the TIGR copyright.

--- src/Converters/ca2ace.pl
+++ src/Converters/ca2ace.pl
@@ -2,27 +2,8 @@
 
 # $Id$
 #
-# Converts from a TIGR .asm file to a new .ace file
+# Converts from a Celera .asm file to a new .ace file
 #
-#  Copyright @ 2002, The Institute for Genomic Research (TIGR).  All
-#  rights reserved.
-
-#  This software is provided "AS IS".  TIGR makes no warranties, express
-#  or implied, including no representation or warranty with respect to
-#  the performance of the software and derivatives or their safety,
-#  effectiveness, or commercial viability.  TIGR does not warrant the
-#  merchantability or fitness of the software and derivatives for any
-#  particular purpose, or that they may be exploited without infringing
-#  the copyrights, patent rights or property rights of others. TIGR shall
-#  not be liable for any claim, demand or action for any loss, harm,
-#  illness or other damage or injury arising from access to or use of the
-#  software or associated information, including without limitation any
-#  direct, indirect, incidental, exemplary, special or consequential
-#  damages.
-
-#  This software program may not be sold, leased, transferred, exported
-#  or otherwise disclaimed to anyone, in whole or in part, without the
-#  prior written consent of TIGR.
 
 use strict;
 use TIGR::Foundation;
