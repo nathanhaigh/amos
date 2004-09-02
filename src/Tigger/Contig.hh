7937f3d9911cae0d3a52c97fc551945d06caab09
dsommer <>
2004-09-02 12:44:16 +0000

    initial creation

---
+++ src/Tigger/Contig.hh
@@ -0,0 +1,33 @@
+#ifndef CONTIG_HH
+#define CONTIG_HH 1
+
+#include <iostream>
+#include "SubGraph.hh"
+#include "INode.hh"
+
+/**
+ * The <b>Contig</b> class
+ *
+ *
+ * <p>Copyright &copy; 2004, The Institute for Genomic Research (TIGR).
+ * <br>All rights reserved.
+ *
+ * @author  Dan Sommer
+ *
+ * <pre>
+ * $RCSfile$
+ * $Revision$
+ * $Date$
+ * $Author$
+ * </pre>
+ */
+class Contig {
+
+public:
+
+  SubGraph* sg;
+  int start_node;
+
+};
+
+#endif // #ifndef CONTIG_HH
