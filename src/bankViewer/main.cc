4ec139240a6f11248f11296644da80e28ed366e3
mschatz <>
2005-03-19 18:16:40 +0000

    Add splash screen

--- src/bankViewer/main.cc
+++ src/bankViewer/main.cc
@@ -1,6 +1,10 @@
-#include <qapplication.h>
-#include "MainWindow.hh"
 #include <iostream>
+#include <qapplication.h>
+#include <qsplashscreen.h>
+#include <qpainter.h>
+
+#include "MainWindow.hh"
+#include "icons/AAI.xpm"
 
 using namespace std;
 
@@ -10,9 +14,21 @@ int main( int argc, char **argv )
 
   if (argc > 4)
   {
-    cerr << "Usage: bankViewer [bankname [contigid]]" << endl;
+    cerr << "Usage: bankViewer [bankname [contigid [position]]]" << endl;
     exit (1);
   }
+
+  QPixmap pixmap((const char **)AAI_xpm);
+  
+  if (argc > 1)
+  {
+    QPainter p (&pixmap);
+    p.drawText(200, 200, 100, 48, Qt::AlignLeft | Qt::AlignBottom, "Waiting for Adam...");
+    p.end();
+  }
+
+  QSplashScreen * splash = new QSplashScreen(pixmap);
+  splash->show();
 
   MainWindow w(NULL, "mainwindow");
   w.setGeometry( 100, 100, 700, 355 );
@@ -33,6 +49,11 @@ int main( int argc, char **argv )
       }
     }
   }
+
+  sleep(1);
+
+  splash->finish(&w);
+  delete splash;
 
   return a.exec();
 }
