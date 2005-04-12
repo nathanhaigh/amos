ccea32505b82ae71f6fdfd9fcf51dada94c0f35f
mschatz <>
2005-04-12 15:04:27 +0000

    Working, but brings more reads than Art's method because it doesn't test if read is already well placed

--- src/Bank/findMissingMates.cc
+++ src/Bank/findMissingMates.cc
@@ -11,6 +11,7 @@
 #include <iostream>
 #include <cassert>
 #include <unistd.h>
+#include <map>
 using namespace std;
 using namespace AMOS;
 
@@ -18,6 +19,12 @@ using namespace AMOS;
 //=============================================================== Globals ====//
 string OPT_BankName;                 // bank name parameter
 bool   OPT_BankSpy = false;          // read or read-only spy
+typedef map <ID_t, ID_t> IDMap;
+
+string contigeid;
+int    rangeStart = -1;
+int    rangeEnd = -1;
+
 
 
 bool hasOverlap(Pos_t rangeStart, // 0-based exact offset of range
@@ -77,9 +84,6 @@ void PrintHelp (const char * s);
 //!
 void PrintUsage (const char * s);
 
-#include <map>
-typedef map <ID_t, ID_t> IDMap;
-
 
 
 //========================================================= Function Defs ====//
@@ -91,14 +95,23 @@ int main (int argc, char ** argv)
   Library_t lib;
   Matepair_t mtp;
 
+  map <ID_t, Distribution_t> libmap;
+  map <ID_t, Pos_t> readlens;
+
+  IDMap mates;
+  IDMap readliblookup;
+  IDMap frgliblookup;
+  IDMap contigscafflookup;
+
+  Scaffold_t scaff;
+  Contig_t contig;
+
   BankStream_t lib_bank (Library_t::NCODE);
   BankStream_t frg_bank (Fragment_t::NCODE);
   BankStream_t mtp_bank (Matepair_t::NCODE);
   BankStream_t ctg_bank (Contig_t::NCODE);
   BankStream_t scf_bank (Scaffold_t::NCODE);
   BankStream_t red_bank (Read_t::NCODE);
-
-  long int cntw = 0;             // written object count
 
   //-- Parse the command line arguments
   ParseArgs (argc, argv);
@@ -114,19 +127,13 @@ int main (int argc, char ** argv)
     ctg_bank . open (OPT_BankName, bm);
     scf_bank . open (OPT_BankName, bm);
 
-    map <ID_t, Distribution_t> libmap;
 
-    //-- Iterate through each library in the bank
     while ( lib_bank >> lib )
     {
       libmap[lib.getIID()] = lib.getDistribution();
     }
-
     cerr << "Indexed " << libmap.size() << " libraries" << endl;
 
-    IDMap mates;
-    IDMap readliblookup;
-    IDMap frgliblookup;
 
     cerr << "Indexing frgs... ";
     while (frg_bank >> frg)
@@ -134,6 +141,7 @@ int main (int argc, char ** argv)
       frgliblookup[frg.getIID()] = frg.getLibrary();
     }
     cerr << frgliblookup.size() << " fragments" << endl;
+
 
     cerr << "Indexing reds... ";
     while (red_bank >> red)
@@ -144,6 +152,8 @@ int main (int argc, char ** argv)
       {
         readliblookup[red.getIID()] = fi->second;
       }
+
+      readlens[red.getIID()] = red.getClearRange().getLength();
     }
     cerr << readliblookup.size() << " reads in fragments" << endl;
 
@@ -151,20 +161,12 @@ int main (int argc, char ** argv)
     cerr << "Indexing mates... ";
     while ( mtp_bank >> mtp )
     {
-      //-- Get the library record for this insert
-      mates[mtp.getReads().first] = mtp.getReads().second;
+      mates[mtp.getReads().first]  = mtp.getReads().second;
       mates[mtp.getReads().second] = mtp.getReads().first;
-
-      readliblookup[mtp.getReads().first] = frg.getLibrary();
-      readliblookup[mtp.getReads().second] = frg.getLibrary();
     }
     cerr << mates.size() << " mates" << endl;
 
 
-    IDMap contigscafflookup;
-
-    Scaffold_t scaff;
-    Contig_t contig;
     cerr << "Indexing scaffolds... ";
     int scaffcount = 0;
     while (scf_bank >> scaff)
@@ -178,33 +180,25 @@ int main (int argc, char ** argv)
         contigscafflookup[ci->source] = scaff.getIID();
       }
     }
-
     cerr << contigscafflookup.size() << " contigs in " 
          << scaffcount << " scaffolds" << endl;
 
 
+
     // load read tiling for scaffold of this contig
-    string contigeid = "1047274138691";
-    int rangeStart = 100;
-    int rangeEnd = 1000;
-
-    // ID_t contigiid = ctg_bank.lookupIID(contigeid.c_str());
-    ID_t contigiid = 92;
-
+    ID_t contigiid = ctg_bank.lookupIID(contigeid.c_str());
     if (contigiid == AMOS::NULL_ID)
     {
       cerr << "contigiid == AMOS::NULL_ID" << endl;
       exit (1);
     }
 
-
-    IDMap::iterator si = contigscafflookup.find(contigiid);
-
     vector <Tile_t> rtiling;
 
+    IDMap::iterator si = contigscafflookup.find(contigiid);
     if (si == contigscafflookup.end())
     {
-      cerr << "Contig not in scaffold, getting tiling" << endl;
+      cerr << "Contig not in scaffold, using original read tiling" << endl;
       ctg_bank.seekg(ctg_bank.getIDMap().lookupBID(contigiid));
       ctg_bank >> contig;
       rtiling = contig.getReadTiling();
@@ -226,7 +220,7 @@ int main (int argc, char ** argv)
 
         int clen = contig.getSeqString().size();
 
-        // map range to scaffold coordinates
+        // map original range to scaffold coordinates
         if (ci->source == contigiid)
         {
           if (ci->range.isReverse())
@@ -267,12 +261,8 @@ int main (int argc, char ** argv)
     }
 
 
-    // test each read to see if it, or its mate falls within range
 
-    cerr << "Testing reads" << endl;
-
-    map <ID_t, bool> readstatus;
-
+    cerr << "Finding reads that overlap [" << rangeStart << "," << rangeEnd << "]" << endl;
     vector<Tile_t>::const_iterator ri;
     for (ri = rtiling.begin(); ri != rtiling.end(); ri++)
     {
@@ -283,50 +273,67 @@ int main (int argc, char ** argv)
         cout << "*";
       }
 
-      cout << ri->source << "\t" << red_bank.lookupEID(ri->source) << "\t" << ri->offset << "\t" << ri->offset+ri->gaps.size()+ri->range.getLength() << endl;
+      cout << "read: " << ri->source 
+           << "\t" << red_bank.lookupEID(ri->source) 
+           << "\t" << ri->offset 
+           << "\t" << ri->offset+ri->gaps.size()+ri->range.getLength()-1 
+           << "\t" << (ri->range.isReverse() ? "R" : "F") 
+           << endl;
 
-      IDMap::const_iterator mi;
-
-      mi = mates.find(ri->source);
-
+      IDMap::const_iterator mi = mates.find(ri->source);
       if (mi != mates.end())
       {
-        int projectedStart;
-        int projectedEnd;
-
         IDMap::const_iterator rli = readliblookup.find(ri->source);
-
         if (rli != readliblookup.end())
         {
-          map<ID_t, Distribution_t>::const_iterator li = libmap.find(ri->source);
-
+          map<ID_t, Distribution_t>::const_iterator li = libmap.find(rli->second);
           if (li != libmap.end())
           {
+            int projectedStart;
+            int projectedEnd;
+
             int NUMSD = 3;
-            int READLEN = 750;
+            int readlen = readlens[mi->second];
 
             if (ri->range.isReverse())
             {
-              projectedStart = ri->offset + ri->range.getLength() + ri->gaps.size() - (li->second.mean + NUMSD*li->second.sd);
-              projectedEnd   = ri->offset + ri->range.getLength() + ri->gaps.size() - (li->second.mean - NUMSD*li->second.sd-READLEN);
+              int roffset = ri->offset + ri->range.getLength() + ri->gaps.size() - 1;
+              projectedStart = roffset - li->second.mean - NUMSD*li->second.sd;
+              projectedEnd   = roffset - li->second.mean + NUMSD*li->second.sd + readlen;
             }
             else
             {
-              projectedStart = ri->offset + li->second.mean - NUMSD*li->second.sd - READLEN;
+              projectedStart = ri->offset + li->second.mean - NUMSD*li->second.sd - readlen;
               projectedEnd   = ri->offset + li->second.mean + NUMSD*li->second.sd;
             }
 
-
             if (hasOverlap(rangeStart, rangeEnd, 
-                           projectedStart, projectedEnd,
+                           projectedStart, projectedEnd - projectedStart + 1,
                            10000000))
             {
-              cout << "*" << endl;
+              cout << "*";
             }
 
-            cout << "mate: " << mi->second << "\t" << red_bank.lookupEID(mi->second) << "\t" << projectedStart << "\t" << projectedEnd;
+            cout << "mate: " << mi->second 
+                 << "\t" << red_bank.lookupEID(mi->second) 
+                 << "\t" << projectedStart 
+                 << "\t" << projectedEnd 
+                 << "\t" << (ri->range.isReverse() ? "F" : "R") 
+                 << endl;
+          }
+          else
+          {
+            cout << "no dist" << endl;
           }
         }
+        else
+        {
+          cout << "no lib" << endl;
+        }
+      }
+      else
+      {
+        cout << "no mate" << endl;
       }
     }
 
@@ -357,7 +364,7 @@ void ParseArgs (int argc, char ** argv)
   int ch, errflg = 0;
   optarg = NULL;
 
-  while ( !errflg && ((ch = getopt (argc, argv, "hsv")) != EOF) )
+  while ( !errflg && ((ch = getopt (argc, argv, "hsvExy")) != EOF) )
     switch (ch)
       {
       case 'h':
@@ -374,16 +381,35 @@ void ParseArgs (int argc, char ** argv)
 	exit (EXIT_SUCCESS);
 	break;
 
+      case 'E':
+         contigeid = argv[optind++];
+         break;
+
+      case 'x':
+          rangeStart = atoi(argv[optind++]);
+          break;
+
+      case 'y':
+          rangeEnd = atoi(argv[optind++]);
+          break;
+
       default:
         errflg ++;
       }
 
-  if ( errflg > 0 || optind != argc - 1 )
-    {
-      PrintUsage (argv[0]);
-      cerr << "Try '" << argv[0] << " -h' for more information.\n";
-      exit (EXIT_FAILURE);
-    }
+  if (errflg > 0 || optind != argc - 1)
+  {
+    PrintUsage (argv[0]);
+    cerr << "Try '" << argv[0] << " -h' for more information.\n";
+    exit (EXIT_FAILURE);
+  }
+
+
+  if (rangeStart == -1 || rangeEnd == -1 || contigeid.empty())
+  {
+    cerr << "Range coordinates and contig id are required" << endl;
+    exit (EXIT_FAILURE);
+  }
 
   OPT_BankName = argv [optind ++];
 }
@@ -399,10 +425,13 @@ void PrintHelp (const char * s)
     << "-h            Display help information\n"
     << "-s            Disregard bank locks and write permissions (spy mode)\n"
     << "-v            Display the compatible bank version\n"
+    << "-E contigeid  Contig eid of interest\n"
+    << "-x start      Start of range\n"
+    << "-y end        End of range\n"
     << endl;
   cerr
-    << "Takes an AMOS bank directory and dumps a bambus .mates files to\n"
-    << "stdout\n\n";
+    << "Finds all reads that should overlap a given contig range. Includes reads that\n"
+    << "should be present by the virtue of their mate and the scaffold\n\n";
   return;
 }
 
