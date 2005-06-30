3a2759fa74fa660d13f9c99fc9875dcae968c778
mschatz <>
2005-06-30 00:28:14 +0000

    Add 1st perl amos tool

---
+++ src/AMOS/DumpCons.pl
@@ -0,0 +1,35 @@
+#!/usr/local/bin/perl -w
+use AMOS;
+
+my $bankpath = shift @ARGV;
+my $type = shift @ARGV;
+my $id = shift @ARGV;
+
+die "Usage: AmosTest bankpath [-e|-i] id\n"
+  if (!defined $id);
+
+my $ctg = AMOS::Contig_t->new();
+my $cbank = AMOS::Bank_t->new($ctg->getNCode());
+
+print STDERR "Opening Bank $bankpath\n";
+$cbank->open($bankpath);
+
+print STDERR "Fetching contig $id\n";
+if ($type =~ /e/)
+{
+  $cbank->fetch($id, $ctg);
+}
+else
+{
+  $cbank->fetch(int($id), $ctg);
+}
+
+my $cons = $ctg->getSeqString();
+print "Cons: $cons\n";
+
+print "Consenus Length: ", length($cons), "\n";
+
+
+
+
+
