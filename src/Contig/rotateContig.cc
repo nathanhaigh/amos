13d47459936f0206e7d3fe9eab5286de125ae964
mschatz <>
2005-08-23 18:49:27 +0000

    Clean up the interface

--- src/Contig/rotateContig.cc
+++ src/Contig/rotateContig.cc
@@ -7,20 +7,83 @@
 using namespace AMOS;
 using namespace std;
 
-bool USE_EID = 1;
+string bank_name;
+
+string contigeid;
+ID_t   contigiid = 0;
+
+Pos_t  gindex = 0;
+Pos_t  uindex = 0;
+
+
+
+void PrintUsage(const char * name)
+{
+  cerr << "Usage: " << name << " [-g gindex | -u uindex] [-E eid | -I iid] bankname" << endl;
+}
+
+void PrintHelp(const char * name)
+{
+  PrintUsage(name);
+}
+
+void ParseArgs (int argc, char ** argv)
+{
+  int ch, errflg = 0;
+  optarg = NULL;
+
+  while ( !errflg && ((ch = getopt (argc, argv, "hvE:I:g:u:")) != EOF) )
+  {
+    switch (ch)
+    {
+      case 'h': PrintHelp (argv[0]); exit (EXIT_SUCCESS); break;
+      case 'v': PrintBankVersion (argv[0]); exit (EXIT_SUCCESS); break;
+
+      case 'E': contigeid = optarg;  break;
+      case 'I': contigiid = atoi(optarg); break;
+      case 'g': gindex    = atoi(optarg); break;
+      case 'u': uindex    = atoi(optarg); break;
+
+      default: errflg ++; 
+    };
+  }
+
+
+  if (errflg || optind != argc - 1)
+  {
+    PrintUsage (argv[0]);
+    cerr << "Try '" << argv[0] << " -h' for more information.\n";
+    exit (EXIT_FAILURE);
+  }
+
+  bank_name = argv[optind++];
+
+  if ((contigeid.empty() && contigiid == NULL_ID) || 
+     (!contigeid.empty() && contigiid != NULL_ID))
+  {
+    cerr << "Must specify one contig id" << endl;
+    exit (EXIT_FAILURE);
+  }
+
+  if ((gindex == 0 && uindex == 0) || (gindex && uindex))
+  {
+    cerr << "Must specify one rotation coordinate" << endl;
+    exit (EXIT_FAILURE);
+  }
+
+  cerr << "Rotating contig eid: " << contigeid << " iid: " << contigiid
+       << " to gindex: " << gindex << " uindex: " << uindex
+       << " in " << bank_name << endl;
+
+}
+
+
 
 int main (int argc, char ** argv)
 {
-  if (argc != 4)
-  {
-    cerr << "Usage: rotateContig bankname eid offset" << endl;
-    return EXIT_FAILURE;
-  }
-
   Bank_t contig_bank(Contig_t::NCODE);
-  string bank_name = argv[1];
-  string contigid = argv[2];
-  Pos_t newoffset = atoi(argv[3]);
+
+  ParseArgs(argc, argv);
 
   Contig_t contig;
 
@@ -29,23 +92,28 @@ int main (int argc, char ** argv)
   try
   {
     contig_bank.open(bank_name, B_READ|B_WRITE);
-    contig_bank.fetch(contigid, contig);
+
+    if (!contigeid.empty()) { contig_bank.fetch(contigeid, contig); }
+    else                    { contig_bank.fetch(contigiid, contig); }
 
     string cons = contig.getSeqString();
     string cqual = contig.getQualString();
 
-    if (newoffset < 0 || newoffset >= cons.length())
+    if (uindex) { gindex = contig.gap2ungap(uindex); }
+
+    if (gindex < 0 || gindex >= cons.length())
     {
-      cerr << "Offset: " << newoffset << " is outside of valid range" << endl;
+      cerr << "Specified offset " << gindex << " is outside of valid range" << endl;
+      contig_bank.close();
       AMOS_THROW("Offset out of range");
     }
 
     // adjust consensus
-    string left  = cons.substr(0, newoffset);
-    string lqual = cqual.substr(0, newoffset);
+    string left  = cons.substr(0, gindex);
+    string lqual = cqual.substr(0, gindex);
 
-    cons  = cons.substr(newoffset, cons.length() - newoffset);
-    cqual = cqual.substr(newoffset, cqual.length() - newoffset);
+    cons  = cons.substr(gindex, cons.length() - gindex);
+    cqual = cqual.substr(gindex, cqual.length() - gindex);
 
     cons += left;
     cqual += lqual;
@@ -55,7 +123,7 @@ int main (int argc, char ** argv)
     vector<Tile_t>::iterator ti;
     for (ti = tiling.begin(); ti != tiling.end(); ti++)
     {
-      ti->offset -= newoffset;
+      ti->offset -= gindex;
       while (ti->offset < 0)
       {
         ti->offset += cons.length();
