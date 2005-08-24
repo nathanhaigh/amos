b321891d884a75545fe0faae5703cae24c2bd017
mschatz <>
2005-08-24 19:10:37 +0000

    Add option to toggle eid/iid, also take optional list of iids or eids to dump

--- src/Bank/bank2contig.cc
+++ src/Bank/bank2contig.cc
@@ -5,7 +5,80 @@
 using namespace AMOS;
 using namespace std;
 
-bool USE_EID = 1;
+bool OPT_UseEIDs = 0;
+bool OPT_UseIIDs = 0;
+string OPT_BankName;
+
+string OPT_EIDFile;
+string OPT_IIDFile;
+
+void PrintUsage (const char * s)
+{
+  cerr << "\nUSAGE: " << s << "  [options]  <bank path>\n\n";
+}
+
+//------------------------------------------------------------- PrintHelp ----//
+void PrintHelp (const char * s)
+{
+  PrintUsage(s);
+  cerr << "-h            Display help information\n"
+       << "-v            Display the compatible bank version\n"
+       << "-e            Use EIDs for names (DEFAULT)\n"
+       << "-i            Use IIDs for names\n"
+       << "-E file       Dump just the contig eids listed in file\n"
+       << "-I file       Dump just the contig iids listed in file\n"
+       << endl;
+  
+  cerr << "Takes an AMOS bank directory and dumps the contigs to stdout\n\n";
+}
+
+void ParseArgs (int argc, char ** argv)
+{
+  int ch, errflg = 0;
+  optarg = NULL;
+
+  while ( !errflg && ((ch = getopt (argc, argv, "hveiE:I:")) != EOF) )
+  {
+    switch (ch)
+    {
+      case 'h':
+        PrintHelp (argv[0]);
+        exit (EXIT_SUCCESS);
+        break;
+
+      case 'v':
+        PrintBankVersion (argv[0]);
+        exit (EXIT_SUCCESS);
+      break;
+
+      case 'e': OPT_UseEIDs = true;   break;
+      case 'i': OPT_UseIIDs = true;   break;
+      case 'E': OPT_EIDFile = optarg; break;
+      case 'I': OPT_IIDFile = optarg; break;
+
+      default: errflg ++;
+      }
+  }
+
+  if ( errflg > 0 || optind != argc - 1 )
+  {
+      PrintUsage (argv[0]);
+      cerr << "Try '" << argv[0] << " -h' for more information.\n";
+      exit (EXIT_FAILURE);
+  }
+
+  OPT_BankName = argv [optind];
+
+  if (!OPT_UseEIDs && !OPT_UseIIDs) { OPT_UseEIDs = true; }
+}
+
+
+
+
+
+
+
+
 
 Pos_t getUngappedPos(const string & str, Pos_t offset)
 {
@@ -22,99 +95,144 @@ Pos_t getUngappedPos(const string & str, Pos_t offset)
   return retval;
 }
 
+void printContig(Contig_t & contig, Bank_t & read_bank)
+{
+  Read_t read; 
+  const std::vector<Tile_t> & tiling = contig.getReadTiling();
+
+  cout << "##";
+
+  if (OPT_UseEIDs) 
+  { 
+    string s(contig.getEID());
+    int i = s.find(' ');
+    if (i != s.npos) { s = s.substr(0,i); }
+    if (s.empty()) { cout << contig.getIID(); }
+    else           { cout << s; }
+  }
+  else 
+  { 
+    cout << contig.getIID(); 
+  }
+
+  const string cons = contig.getSeqString();
+
+  cout << " "  << tiling.size()
+       << " "  << cons.length()
+       << " bases, 00000000 checksum." << endl;
+
+  Fasta_Print(stdout, cons.c_str(), NULL, 60);
+
+  vector<Tile_t>::const_iterator i;
+  for (i =  tiling.begin();
+       i != tiling.end();
+       i++)
+  {
+    bool rc = 0;
+    read_bank.fetch(i->source, read);
+    Range_t range = i->range;
+    Range_t clr = range;
+    Pos_t gappedLen = i->range.getLength() + i->gaps.size();
+
+    if (i->range.begin > i->range.end) { clr.end++; rc = 1; } 
+    else                               { clr.begin++; }
+
+    cout << "#";
+
+    if (OPT_UseEIDs) 
+    { 
+      string s(read.getEID());
+      int i = s.find(' ');
+      if (i != s.npos) { s = s.substr(0,i); }
+      cout << s;
+    }
+    else 
+    { 
+      cout << read.getIID(); 
+    }
+
+    cout << "(" << i->offset 
+         << ((rc) ? ") [RC] " : ") [] ") << gappedLen
+         << " bases, 00000000 checksum."
+         << " {" << clr.begin << " " << clr.end << "}"
+         << " <" << getUngappedPos(cons, i->offset)
+         << " "  << getUngappedPos(cons, i->offset + gappedLen - 1)
+         << ">"  << endl;
+
+    if (rc) { range.swap(); }
+    string sequence = read.getSeqString(range);
+    if (rc) { Reverse_Complement(sequence); }
+
+    Pos_t gapcount = 0;
+
+    vector<Pos_t>::const_iterator g;
+    for (g  = i->gaps.begin();
+         g != i->gaps.end();
+         g++)
+    {
+      sequence.insert(*g+gapcount, "-", 1);
+      gapcount++;
+    }
+
+    Fasta_Print(stdout, sequence.c_str(), NULL, 60);
+  }
+}
+
 int main (int argc, char ** argv)
 {
-  if (argc != 2)
-  {
-    cerr << "Usage: bank2contig bankname" << endl;
-    return EXIT_FAILURE;
-  }
+  ParseArgs (argc, argv);
 
   BankStream_t contig_bank(Contig_t::NCODE);
   Bank_t read_bank(Read_t::NCODE);
-  string bank_name = argv[1];
 
-  cerr << "Processing " << bank_name << " at " << Date() << endl;
+  cerr << "Processing " << OPT_BankName << " at " << Date() << endl;
 
   try
   {
-    read_bank.open(bank_name, B_READ);
-    contig_bank.open(bank_name, B_READ);
+    read_bank.open(OPT_BankName, B_READ);
+    contig_bank.open(OPT_BankName, B_READ);
 
     Contig_t contig;
-    Read_t read; 
-    
-    while (contig_bank >> contig)
+    ifstream file;
+    string id;
+
+    if (!OPT_EIDFile.empty())
     {
-      const std::vector<Tile_t> & tiling = contig.getReadTiling();
-
-      cout << "##";
-
-      if (USE_EID) 
-      { 
-        string s(contig.getEID());
-        if (s.empty()) { cout << contig.getIID(); }
-        else           { cout << s; }
-      }
-      else { cout << contig.getIID(); }
-
-      const string cons = contig.getSeqString();
-
-      cout << " "  << tiling.size()
-           << " "  << cons.length()
-           << " bases, 00000000 checksum." << endl;
-
-      Fasta_Print(stdout, cons.c_str(), NULL, 60);
-
-      vector<Tile_t>::const_iterator i;
-      for (i =  tiling.begin();
-           i != tiling.end();
-           i++)
+      file.open(OPT_EIDFile.c_str());
+      
+      if (!file)
       {
-        bool rc = 0;
-        read_bank.fetch(i->source, read);
-        Range_t range = i->range;
-        Range_t clr = range;
-        Pos_t gappedLen = i->range.getLength() + i->gaps.size();
+        throw Exception_t("Couldn't open EID File", __LINE__, __FILE__);
+      }
 
-        if (i->range.begin > i->range.end) { clr.end++; rc = 1; } 
-        else                               { clr.begin++; }
+      while (file >> id)
+      {
+        contig_bank.seekg(contig_bank.getIDMap().lookupBID(id));
+        contig_bank >> contig;
+        printContig(contig, read_bank);
+      }
+    }
+    else if (!OPT_IIDFile.empty())
+    {
+      file.open(OPT_IIDFile.c_str());
 
-        cout << "#";
+      if (!file)
+      {
+        throw Exception_t("Couldn't open IID File", __LINE__, __FILE__);
+      }
 
-        if (USE_EID) 
-        { 
-          string s(read.getEID());
-          int i = s.find(' ');
-          if (i != s.npos) { s = s.substr(0,i); }
-          cout << s;
-        }
-        else { cout << read.getIID(); }
-
-        cout << "(" << i->offset 
-             << ((rc) ? ") [RC] " : ") [] ") << gappedLen
-             << " bases, 00000000 checksum."
-             << " {" << clr.begin << " " << clr.end << "}"
-             << " <" << getUngappedPos(cons, i->offset)
-             << " "  << getUngappedPos(cons, i->offset + gappedLen - 1)
-             << ">"  << endl;
-
-        if (rc) { range.swap(); }
-        string sequence = read.getSeqString(range);
-        if (rc) { Reverse_Complement(sequence); }
-
-        Pos_t gapcount = 0;
-
-        vector<Pos_t>::const_iterator g;
-        for (g  = i->gaps.begin();
-             g != i->gaps.end();
-             g++)
-        {
-          sequence.insert(*g+gapcount, "-", 1);
-          gapcount++;
-        }
-
-        Fasta_Print(stdout, sequence.c_str(), NULL, 60);
+      while (file >> id)
+      {
+        contig_bank.seekg(contig_bank.getIDMap().lookupBID(atoi(id.c_str())));
+        contig_bank >> contig;
+        printContig(contig, read_bank);
+      }
+    }
+    else
+    {
+      while (contig_bank >> contig)
+      {
+        printContig(contig, read_bank);
       }
     }
 
