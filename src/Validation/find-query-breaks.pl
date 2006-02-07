b083d3d11a58b6be9547f6be42291fc4c2ca9409
mcschatz <>
2006-02-07 20:09:22 +0000

    Add options for flagging various broken alignments

--- src/Validation/find-query-breaks.pl
+++ src/Validation/find-query-breaks.pl
@@ -6,6 +6,8 @@ use AMOS::AmosFoundation;
 my $TRIMWIGGLE = 10;
 my $CIRCLEWIGGLE = 0;
 my $MINLENGTH = 0;
+my $FLAGDIST = undef;
+my $FLAGSAME = undef;
 
 my $HELPTEXT = qq~
 Find alignment breaks in query sequences
@@ -15,13 +17,15 @@ Find alignment breaks in query sequences
   -t <val> Minimum length of alignment break to report (Default: 10)
   -c <val> Minimum distance to edge of reference sequence to report (Default: 0)
   -l <val> Minimum length of query sequence to report (Default: 0)
+  -f <val> Flag broken alignments within this distance of reference
+  -s       Flag adjacent broken alignments from same query
 ~;
 
 sub printAlignment
 {
   my $align = shift;
   my $rc = ($align->{qrc}) ? "[rc]" : "[]";
-  print "$align->{rid} {1 [$align->{rstart},$align->{rend}] $align->{rlen}} | $align->{qid} {1 [$align->{qstart},$align->{qend}] $align->{qlen}} $rc ";
+  print "$align->{rid} {1 [$align->{rstart},$align->{rend}] $align->{rlen}} | $align->{qid} {1 [$align->{qstart},$align->{qend}] $align->{qlen}} $rc";
 }
 
 my $base = new AMOS::AmosFoundation;
@@ -30,7 +34,9 @@ $base->setUsage("find-query-breaks.pl [options] deltafile");
 
 my $err = $base->getOptions("t=i" => \$TRIMWIGGLE,
                             "c=i" => \$CIRCLEWIGGLE,
-                            "l=i" => \$MINLENGTH);
+                            "l=i" => \$MINLENGTH,
+                            "f=i" => \$FLAGDIST,
+                            "s"   => \$FLAGSAME);
 
 if (!$err || 0 == scalar @ARGV) { $base->bail("Command line parsing failed. See -h option"); }
 
@@ -42,6 +48,8 @@ foreach my $deltafile (@ARGV)
 
   my $cmd = "show-coords -Hcrl $deltafile";
   open COORDS, "$cmd |" or die "Can't run $cmd ($!)\n";
+
+  my $lastalign = undef;
 
   while (<COORDS>)
   {
@@ -78,6 +86,29 @@ foreach my $deltafile (@ARGV)
 
     next if ($align->{qlen} < $MINLENGTH);
 
+    if (defined $lastalign)
+    {
+      my $dist = $align->{rstart} - $lastalign->{rend};
+ #     print "lid: $lastalign->{rid} id: $align->{rid} dist: $dist\n"
+    }
+
+    my $flag = "";
+    if ((defined $FLAGDIST) &&
+        (defined $lastalign) &&
+        ($lastalign->{rid} eq $align->{rid}) &&
+        ($align->{rstart} - $lastalign->{rend} < $FLAGDIST))
+    {
+      $flag = "***";
+    }
+
+    if ((defined $lastalign) &&
+        (defined $FLAGSAME) &&
+        ($lastalign->{rid} eq $align->{rid}) &&
+        ($lastalign->{qid} eq $align->{qid})) 
+    {
+      $flag = "***";
+    }
+        
 
     my $breakcount = 0;
 
@@ -92,7 +123,7 @@ foreach my $deltafile (@ARGV)
     {
       my $dist = $align->{qstart} - 1;
       printAlignment($align);
-      print "Start Break: $dist\n";
+      print " Start Break: $dist $flag\n";
       $breakcount++;
     }
 
@@ -106,8 +137,13 @@ foreach my $deltafile (@ARGV)
     {
       my $dist = $align->{qlen} - $align->{qend};
       printAlignment($align);
-      print "End Break: $dist\n";
+      print " End Break: $dist $flag\n";
       $breakcount++;
+    }
+
+    if ($breakcount)
+    {
+      $lastalign = $align;
     }
 
     #print $_ if $breakcount;
