df04b2a99a159f3fd4518f33b002597ccce72139
mcschatz <>
2006-02-08 04:30:01 +0000

    Add option to print features in AMOS style

--- src/Validation/find-query-breaks.pl
+++ src/Validation/find-query-breaks.pl
@@ -8,18 +8,22 @@ my $CIRCLEWIGGLE = 0;
 my $MINLENGTH = 0;
 my $FLAGDIST = undef;
 my $FLAGSAME = undef;
+my $DOAMOS = 0;
 
 my $HELPTEXT = qq~
 Find alignment breaks in query sequences
 
   find-query-breaks.pl [options] deltafile
 
-  -t <val> Minimum length of alignment break to report (Default: 10)
+  -b <val> Minimum length of alignment break to report (Default: 10)
   -c <val> Minimum distance to edge of reference sequence to report (Default: 0)
   -l <val> Minimum length of query sequence to report (Default: 0)
   -f <val> Flag broken alignments within this distance of reference
   -s       Flag adjacent broken alignments from same query
+
+  -a       Display breaks as AMOS features
 ~;
+
 
 sub printAlignment
 {
@@ -32,13 +36,20 @@ my $base = new AMOS::AmosFoundation;
 $base->setHelpText($HELPTEXT);
 $base->setUsage("find-query-breaks.pl [options] deltafile");
 
-my $err = $base->getOptions("t=i" => \$TRIMWIGGLE,
+my $err = $base->getOptions("b=i" => \$TRIMWIGGLE,
                             "c=i" => \$CIRCLEWIGGLE,
                             "l=i" => \$MINLENGTH,
                             "f=i" => \$FLAGDIST,
-                            "s"   => \$FLAGSAME);
+                            "s"   => \$FLAGSAME,
+                            "a"   => \$DOAMOS);
 
-if (!$err || 0 == scalar @ARGV) { $base->bail("Command line parsing failed. See -h option"); }
+if (!$err) { $base->bail("Command line parsing failed. See -h option"); }
+
+if (scalar @ARGV == 0)
+{
+  print "Usage: ".$base->getUsage()."\n";
+  exit(0);
+}
 
 my $multidelta = (scalar @ARGV) > 1;
 
@@ -122,9 +133,20 @@ foreach my $deltafile (@ARGV)
     elsif (($align->{qstart} - 1) > $TRIMWIGGLE)
     {
       my $dist = $align->{qstart} - 1;
-      printAlignment($align);
-      print " Start Break: $dist $flag\n";
       $breakcount++;
+
+      if ($DOAMOS)
+      {
+        my $s = $align->{qstart};
+        my $e = $s+1;
+        
+        print "$align->{qid}\tB\t$e\t$s\tSTART_BREAK: $dist\n";
+      }
+      else
+      {
+        printAlignment($align);
+        print " Start Break: $dist $flag\n";
+      }
     }
 
     if ((!$align->{qrc} && ($align->{rlen} - $align->{rend} <= $CIRCLEWIGGLE)) ||
@@ -136,9 +158,20 @@ foreach my $deltafile (@ARGV)
     elsif (($align->{qlen} - $align->{qend}) > $TRIMWIGGLE)
     {
       my $dist = $align->{qlen} - $align->{qend};
-      printAlignment($align);
-      print " End Break: $dist $flag\n";
       $breakcount++;
+
+      if ($DOAMOS)
+      {
+        my $s = $align->{qend};
+        my $e = $s+1;
+        
+        print "$align->{qid}\tB\t$s\t$e\tEND_BREAK: $dist\n";
+      }
+      else
+      {
+        printAlignment($align);
+        print " End Break: $dist $flag\n";
+      }
     }
 
     if ($breakcount)
