8e28cd9eb9d5f00fbc69a4993865d500db677462
mcschatz <>
2006-03-30 02:01:22 +0000

    Add simple contig loader

---
+++ src/Bank/simpleContigLoader.cc
@@ -0,0 +1,139 @@
+#include "foundation_AMOS.hh"
+#include <iostream>
+#include <cassert>
+#include <unistd.h>
+using namespace std;
+using namespace AMOS;
+
+
+//=============================================================== Globals ====//
+string OPT_BankName; 
+string OPT_ContigFile;
+
+
+
+
+//========================================================== Fuction Decs ====//
+//----------------------------------------------------- ParseArgs --------------
+//! \brief Sets the global OPT_% values from the command line arguments
+//!
+//! \return void
+//!
+void ParseArgs (int argc, char ** argv);
+
+
+//----------------------------------------------------- PrintHelp --------------
+//! \brief Prints help information to cerr
+//!
+//! \param s The program name, i.e. argv[0]
+//! \return void
+//!
+void PrintHelp (const char * s);
+
+
+//----------------------------------------------------- PrintUsage -------------
+//! \brief Prints usage information to cerr
+//!
+//! \param s The program name, i.e. argv[0]
+//! \return void
+//!
+void PrintUsage (const char * s);
+
+
+
+//========================================================= Function Defs ====//
+int main (int argc, char ** argv)
+{
+  int exitcode = EXIT_SUCCESS;
+
+  Bank_t read_bank (Read_t::NCODE);
+  Bank_t contig_bank(Contig_t::NCODE);
+
+  long int cntw = 0;             // written object count
+
+  //-- Parse the command line arguments
+  ParseArgs (argc, argv);
+
+  //-- BEGIN: MAIN EXCEPTION CATCH
+  try {
+
+    read_bank.open (OPT_BankName, B_READ|B_SPY);
+    contig_bank.open(OPT_BankName, B_READ|B_WRITE);
+
+  }
+  catch (const Exception_t & e) {
+    cerr << "FATAL: " << e . what( ) << endl
+         << "  there has been a fatal error, abort" << endl;
+    exitcode = EXIT_FAILURE;
+  }
+  //-- END: MAIN EXCEPTION CATCH
+
+  return exitcode;
+}
+
+
+
+
+//------------------------------------------------------------- ParseArgs ----//
+void ParseArgs (int argc, char ** argv)
+{
+  int ch, errflg = 0;
+  optarg = NULL;
+
+  while ( !errflg && ((ch = getopt (argc, argv, "hv")) != EOF) )
+    switch (ch)
+      {
+
+      case 'h':
+        PrintHelp (argv[0]);
+        exit (EXIT_SUCCESS);
+        break;
+
+      case 'v':
+	PrintBankVersion (argv[0]);
+	exit (EXIT_SUCCESS);
+	break;
+
+      default:
+        errflg ++;
+      }
+
+  if ( errflg > 0 || optind != argc - 1 )
+    {
+      PrintUsage (argv[0]);
+      cerr << "Try '" << argv[0] << " -h' for more information.\n";
+      exit (EXIT_FAILURE);
+    }
+
+  OPT_BankName = argv [optind ++];
+}
+
+
+
+
+//------------------------------------------------------------- PrintHelp ----//
+void PrintHelp (const char * s)
+{
+  PrintUsage (s);
+  cerr
+    << "-e            Report objects by EID instead of IID\n"
+    << "-h            Display help information\n"
+    << "-s            Disregard bank locks and write permissions (spy mode)\n"
+    << "-v            Display the compatible bank version\n"
+    << endl;
+  cerr
+    << "Takes an AMOS bank directory and dumps a bambus .mates files to\n"
+    << "stdout\n\n";
+  return;
+}
+
+
+
+
+//------------------------------------------------------------ PrintUsage ----//
+void PrintUsage (const char * s)
+{
+  cerr
+    << "\nUSAGE: " << s << "  [options]  <bank path>\n\n";
+  return;
+}
