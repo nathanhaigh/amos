6ea41a96483d6c44833e29d22d1f310f49c30762
mcschatz <>
2006-09-22 21:39:06 +0000

    Set k=0 reports on library, regardless of name. Other cleanups

--- src/bankViewer/insert-sizes.cc
+++ src/bankViewer/insert-sizes.cc
@@ -1,17 +1,9 @@
-////////////////////////////////////////////////////////////////////////////////
-//! \file
-//! \author Adam M Phillippy
-//! \date 03/08/2004
-//!
-//! \brief Dumps a bambus .mates file from an AMOS bank
-//!
-////////////////////////////////////////////////////////////////////////////////
-
 #include "foundation_AMOS.hh"
 #include <iostream>
 #include <cassert>
 #include <unistd.h>
 #include <map>
+#include <set>
 #include <cmath>
 
 #include "DataStore.hh"
@@ -21,46 +13,23 @@
 using namespace std;
 using namespace AMOS;
 
-typedef HASHMAP::hash_map<ID_t, Tile_t *> SeqTileMap_t;
 
-int FRAGMENTSTRLEN = 3;
-int m_verbose = 0;
+string OPT_BankName;
+bool   OPT_BankSpy = false; 
+int    OPT_SUBLIBRARYLEN = 0;
+int    OPT_DETAILS = false;
+int    OPT_PRINTSUMMARY = true;
+
 
 DataStore * m_datastore;
 int m_connectMates = 1;
 typedef std::vector<Insert *> InsertList_t;
 InsertList_t m_inserts;
-map <string, InsertStats *> fragstats;
-
-//=============================================================== Globals ====//
-string OPT_BankName;                 // bank name parameter
-bool   OPT_BankSpy = false;          // read or read-only spy
+map <string, InsertStats *> libstats;
 
 
-//========================================================== Fuction Decs ====//
-//----------------------------------------------------- ParseArgs --------------
-//! \brief Sets the global OPT_% values from the command line arguments
-//!
-//! \return void
-//!
 void ParseArgs (int argc, char ** argv);
-
-
-//----------------------------------------------------- PrintHelp --------------
-//! \brief Prints help information to cerr
-//!
-//! \param s The program name, i.e. argv[0]
-//! \return void
-//!
 void PrintHelp (const char * s);
-
-
-//----------------------------------------------------- PrintUsage -------------
-//! \brief Prints usage information to cerr
-//!
-//! \param s The program name, i.e. argv[0]
-//! \return void
-//!
 void PrintUsage (const char * s);
 
 void processInserts()
@@ -74,11 +43,21 @@ void processInserts()
   {
     if ((*i)->m_active == 2)
     {
-      string fragment = m_datastore->read_bank.lookupEID((*i)->m_aid).substr(0,FRAGMENTSTRLEN);
+      string fragment;
 
-      if (m_verbose)
+      if (OPT_SUBLIBRARYLEN == 0)
       {
+        char buffer[16];
+        sprintf(buffer, "%d", (*i)->m_libid);
+        fragment=buffer;
+      }
+      else
+      {
+        fragment = m_datastore->read_bank.lookupEID((*i)->m_aid).substr(0,OPT_SUBLIBRARYLEN);
+      }
 
+      if (OPT_DETAILS)
+      {
         ID_t fragid = m_datastore->lookupFragId((*i)->m_aid);
 
         cout << (*i)->m_libid << "\t"
@@ -89,15 +68,18 @@ void processInserts()
              << (*i)->m_actual << endl;
       }
 
-      fi = fragstats.find(fragment);
-      if (fi == fragstats.end())
+      if (OPT_PRINTSUMMARY)
       {
-        char buffer[16];
-        sprintf(buffer, "%d", (*i)->m_libid);
-        fi = fragstats.insert(make_pair(fragment, new InsertStats(buffer))).first;
-      }
+        fi = libstats.find(fragment);
+        if (fi == libstats.end())
+        {
+          char buffer[16];
+          sprintf(buffer, "%d", (*i)->m_libid);
+          fi = libstats.insert(make_pair(fragment, new InsertStats(buffer))).first;
+        }
 
-      fi->second->addSize((*i)->m_actual);
+        fi->second->addSize((*i)->m_actual);
+      }
     }
 
     delete (*i);
@@ -135,38 +117,66 @@ int main (int argc, char ** argv)
 
     if (m_datastore->scaffold_bank.isOpen())
     {
-      cerr << "Processing scaffolds... ";
+      cerr << "Processing scaffolds..." << endl;
+
+      set<ID_t> seencontigs;
 
       m_datastore->scaffold_bank.seekg(1);
       while (m_datastore->scaffold_bank >> scaff)
       {
         vector <Tile_t> rtiling;
         m_datastore->mapReadsToScaffold(scaff, rtiling, 1);
-        m_datastore->calculateInserts(rtiling, m_inserts, m_connectMates, 1);
+        m_datastore->calculateInserts(rtiling, m_inserts, m_connectMates, 0);
         processInserts();
+
+        vector<Tile_t>::iterator ci;
+        for (ci = scaff.getContigTiling().begin();
+             ci != scaff.getContigTiling().end();
+             ci++)
+        {
+          seencontigs.insert(ci->source);
+        }
+      }
+
+      cerr << "Processing unplaced contigs..." << endl;
+      m_datastore->contig_bank.seekg(1);
+
+      set<ID_t>::iterator si;
+
+      while (m_datastore->contig_bank >> contig)
+      {
+        si = seencontigs.find(contig.getIID());
+
+        if (si == seencontigs.end())
+        {
+          m_datastore->calculateInserts(contig.getReadTiling(), m_inserts, m_connectMates, 0);
+          processInserts();
+        }
       }
     }
     else
     {
-      cerr << "Processing contigs... ";
+      cerr << "Processing contigs..." << endl;
 
       m_datastore->contig_bank.seekg(1);
       while (m_datastore->contig_bank >> contig)
       {
-        m_datastore->calculateInserts(contig.getReadTiling(), m_inserts, m_connectMates, 1);
+        m_datastore->calculateInserts(contig.getReadTiling(), m_inserts, m_connectMates, 0);
         processInserts();
       }
     }
 
-    cout << endl;
-    cout << "SubLibrary Summaries" << endl;
-    for (fi = fragstats.begin(); fi != fragstats.end(); fi++)
+    if (OPT_PRINTSUMMARY)
     {
-      cout << fi->second->m_label << "\t"
-           << fi->first << "\t"
-           << fi->second->count() << "\t"
-           << fi->second->mean() << "\t"
-           << fi->second->stdev() << endl;
+      cout << "SubLibrary (k=" << OPT_SUBLIBRARYLEN <<") Summaries" << endl;
+      for (fi = libstats.begin(); fi != libstats.end(); fi++)
+      {
+        cout << fi->second->m_label << "\t"
+             << fi->first << "\t"
+             << fi->second->count() << "\t"
+             << fi->second->mean() << "\t"
+             << fi->second->stdev() << endl;
+      }
     }
 
   }
@@ -190,23 +200,21 @@ void ParseArgs (int argc, char ** argv)
   int ch, errflg = 0;
   optarg = NULL;
 
-  while ( !errflg && ((ch = getopt (argc, argv, "hsdvf:")) != EOF) )
+  while ( !errflg && ((ch = getopt (argc, argv, "Shsdvf:")) != EOF) )
+  {
     switch (ch)
-      {
+    {
       case 'h': PrintHelp (argv[0]); exit (EXIT_SUCCESS); break;
-
-      case 's': OPT_BankSpy = true; break;
-
       case 'v': PrintBankVersion (argv[0]); exit (EXIT_SUCCESS); break;
-
-      case 'f': FRAGMENTSTRLEN = atoi(optarg); break;
-
-      case 'd': m_verbose = 1; break;
-
+      case 's': OPT_BankSpy = true; break;
+      case 'd': OPT_DETAILS = true; break;
+      case 'S': OPT_PRINTSUMMARY = false; break;
+      case 'f': OPT_SUBLIBRARYLEN = atoi(optarg); break;
 
       default:
         errflg ++;
-      }
+    };
+  }
 
   if (errflg > 0 || optind != argc - 1)
   {
@@ -227,11 +235,25 @@ void PrintHelp (const char * s)
 {
   PrintUsage (s);
   cerr
+    << "Options:\n"
     << "-h            Display help information\n"
     << "-s            Disregard bank locks and write permissions (spy mode)\n"
     << "-v            Display the compatible bank version\n"
-    << "-f len        Number of characters of seqname to use as sublibrary (default=3)\n"
+    << "-f len        Number of characters of seqname to use as sublibrary (default=0)\n"
     << "-d            Show details for each insert\n"
+    << "-S            Don't print library summaries\n"
+    << "\n"
+    << "Print information on the observed sizes of inserts grouped by library\n"
+    << "or sublibrary. Both reads have to be in the same scaffold (or unplaced\n"
+    << "contig) for the insert to be reported. A sublibrary is defined by the\n"
+    << "first k characters of the sequence name. By default, k=0 and it\n"
+    << "reports on the whole library, use -f to set a new length.\n"
+    << "\n"
+    << "By default, only print the SubLibrary summaries:\n"
+    << "Libid SubLibrary Count Mean StDev\n"
+    << "\n"
+    << "Use details mode (-d) to also print the size for all inserts:\n"
+    << "Libid FragmentID SubLibrary Read1 Read2 Size\n"
     << endl;
   return;
 }
