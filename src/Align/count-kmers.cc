c0d36ed4fac052516ec348f7d09963de186c7ff9
mcschatz <>
2007-01-11 03:39:05 +0000

    Store mers as bit vectors

--- src/Align/count-kmers.cc
+++ src/Align/count-kmers.cc
@@ -16,12 +16,10 @@ int BAD_CHAR = 0;
 int PRINT_SIMPLE = 0;
 
 
-#define  DEBUG  0
-
 #define BIGMER 1
 
 #ifdef BIGMER
-typedef string Mer_t;
+typedef vector<bool> Mer_t;
 typedef map<Mer_t, unsigned int> MerTable_t;
 #else
 typedef long long unsigned Mer_t;
@@ -29,7 +27,6 @@ typedef hash_map<Mer_t, unsigned int, hash<unsigned long> > MerTable_t;
 #endif
 
 static Mer_t Forward_Mask;
-static Mer_t MerMask;
 static int   Kmer_Len = 22;
 
 static unsigned Char_To_Binary (char ch);
@@ -100,14 +97,15 @@ int  main (int argc, char * argv [])
       exit(1);
     }
 
+
+    #ifndef BIGMER
     if (Kmer_Len > 31 || Kmer_Len < 1)
     {
       cerr << "Kmer length must be <= 31" << endl;
       exit(1);
     }
-
     Forward_Mask = ((long long unsigned) 1 << (2 * Kmer_Len - 2)) - 1;
-    MerMask = ((long long unsigned) 1 << (2 * Kmer_Len)) - 1;
+    #endif
 
     MerTable_t mer_table;
 
@@ -128,7 +126,9 @@ int  main (int argc, char * argv [])
       while  (Fasta_Read (fp, s, tag))
       {
         CountMers(s, mer_table);
+        cerr << ".";
       }
+      cerr << endl;
     }
     else if (!readbank.empty())
     {
@@ -240,34 +240,20 @@ const char * bintoascii = "ACGT";
 
 
 
-static unsigned  Char_To_Binary
-    (char ch)
-
 //  Return the binary equivalent of  ch .
-
+static unsigned  Char_To_Binary (char ch)
+{
+  switch  (tolower (ch))
   {
-   switch  (tolower (ch))
-     {
-      case  'a' :
-        return  0;
-      case  'c' :
-        return  1;
-      case  'g' :
-        return  2;
-      case  't' :
-        return  3;
-      default:
-        BAD_CHAR++;
-        return 0;
-        /*
-      default :
-        sprintf (Clean_Exit_Msg_Line, "Bad char = %c (ASCII %u) in Char_To_Binary",
-             ch, unsigned (ch));
-        Clean_Exit (Clean_Exit_Msg_Line, __FILE__, __LINE__);
-        */
-     }
-   return  0;
-  }
+    case  'a' : return  0;
+    case  'c' : return  1;
+    case  'g' : return  2;
+    case  't' : return  3;
+    default: BAD_CHAR++; return 0;
+  };
+
+  return  0;
+}
 
 
 char RC(char ch)
@@ -304,21 +290,39 @@ char NORM(char ch)
 
 void InitMer(Mer_t & mer)
 {
-  mer.erase();
-  mer.resize(Kmer_Len);
+  mer.clear();
+  mer.resize(Kmer_Len*2);
 }
 
 static void MerToAscii(Mer_t mer, string & s)
 {
-  s = mer;
+  s.erase();
+  s.resize(Kmer_Len);
+
+  for (int i = 0; i < Kmer_Len; i++)
+  {
+    char m = 0;
+    m |= mer[i*2+1] << 1;
+    m |= mer[i*2];
+
+    s[Kmer_Len-i-1] = bintoascii[m];
+  }
 }
 
 //  Add  ch  to  mer  on the right, sliding one character
 //  off the left end of  mer .
 static void  Forward_Add_Ch(Mer_t & mer, char ch)
 {
-  mer += NORM(ch);
-  if (mer.length() > Kmer_Len) { mer.erase(0,1); }
+  // shift left
+  for (int i = Kmer_Len * 2-1; i > 1; i--)
+  {
+    mer[i] = mer[i-2];
+  }
+
+  // append to end
+  ch = Char_To_Binary(ch);
+  mer[1] = ch & 2;
+  mer[0] = ch & 1;
 }
 
 
@@ -326,9 +330,16 @@ static void  Forward_Add_Ch(Mer_t & mer, char ch)
 //  sliding one character off the right end of  mer .
 static void  Reverse_Add_Ch(Mer_t & mer, char ch)
 {
-  char rc = RC(ch);
-  mer.insert(0, 1, rc);
-  if (mer.length() > Kmer_Len) { mer.resize(Kmer_Len); }
+  // shift right
+  for (int i = 0; i < Kmer_Len*2-2; i++)
+  {
+    mer[i] = mer[i+2];
+  }
+
+  // append to front
+  ch = 3^Char_To_Binary(ch);
+  mer[Kmer_Len*2-1] = ch&2;
+  mer[Kmer_Len*2-2] = ch&1;
 }
 
 
@@ -366,7 +377,6 @@ static void  Reverse_Add_Ch(Mer_t & mer, char ch)
 {
   mer >>= 2;
   mer |= ((long long unsigned) (3 ^ Char_To_Binary (ch)) << (2 * Kmer_Len - 2));
-  mer &= MerMask;
 }
 
 #endif
@@ -400,7 +410,12 @@ static void  CountMers (const string & s, MerTable_t & mer_table)
      Forward_Add_Ch (fwd_mer, s [i]);
      Reverse_Add_Ch (rev_mer, s [i]);
 
-     if (fwd_mer < rev_mer)
+     string f,r;
+
+     MerToAscii(fwd_mer, f);
+     MerToAscii(rev_mer, r);
+
+     if (f < r)
      {
        fi = mer_table.find(fwd_mer);
        if (fi == mer_table.end()) { fi=mer_table.insert(make_pair(fwd_mer,0)).first; }
