8033677ddf3ba21d7eb8fe15e62ddb333dafb25f
mcschatz <>
2009-09-22 22:35:36 +0000

    make sure reads are only written once

--- src/Converters/stream_compute_tcov.pl
+++ src/Converters/stream_compute_tcov.pl
@@ -2,7 +2,13 @@
 use strict;
 
 my $redcnt = 0;
-my $MAXREADS = 40;
+my $dupread = 0;
+my %seenreads;
+my $MAXREADS = 20000;
+
+my $TOAMOS       = "./toAmos";
+my $BANKTRANSACT = "./bank-transact";
+my $ANALYZESNPS  = "./analyzeSNPs";
 
 my $pid = $$;
 
@@ -25,27 +31,59 @@ sub printTCov
     close FRG;
     close ASM;
 
-    my $cmd0 = "(toAmos -f $pid.frg -a $pid.asm -o - | "
-             . "bank-transact -m - -b $pid.bnk -c) >& /dev/null";
+    my $cmd0 = "($TOAMOS -f $pid.frg -a $pid.asm -o - | "
+             . "$BANKTRANSACT -m - -b $pid.bnk -c)";
 
     my $rc0 = system($cmd0);
-    if ($rc0) { die "Can't run $cmd0 for $ctgid ($!)\n"; }
+    if ($rc0) 
+    { 
+      system("cat $pid.frg 1>&2");
+      system("cat $pid.asm 1>&2");
+      
+      die "Can't run $cmd0 for $ctgid ($!)\n"; 
+    }
 
-    my $cmd1 = "analyzeSNPs  -b $pid.bnk/ -T -e -a | tr ' ' '\t'";
+    my $cmd1 = "($ANALYZESNPS  -b $pid.bnk/ -T -e -a | tr ' ' '\t') 2> $pid.err";
     my $rc1 = system($cmd1);
     if ($rc1) { die "Can't run $cmd1 for $ctgid ($!)\n"; }
 
     system("rm -rf $pid.bnk");
 
+    open ERR, "< $pid.err" or die "Can't open $pid.err ($!)\n";
+    while (<ERR>)
+    {
+      print STDERR $_;
+      if (/(\d+) positions reported of (\d+) total SNPs found./)
+      {
+        my $pos  = $1;
+        my $snps = $2;
+
+        print STDERR "reporter:counter:tcov,positions,$pos\n";
+        print STDERR "reporter:counter:tcov,snps,$snps\n";
+      }
+      elsif (/Searched \d+ positions in (\d+) contigs./)
+      {
+        my $ctgs = $1;
+        print STDERR "reporter:counter:tcov,contigs,$ctgs\n";
+      }
+    }
+
+    system("rm -f $pid.err");
+
     openFiles();
 
     $redcnt = 0;
+    undef %seenreads;
   }
 }
 
 
 my $ctgid;
 openFiles();
+
+system("chmod +x $TOAMOS");
+system("chmod +x $BANKTRANSACT");
+system("chmod +x $ANALYZESNPS");
 
 while (<>)
 {
@@ -73,11 +111,39 @@ while (<>)
     my $acc = shift @vals;
     my $ft  = shift @vals;
 
-    $redcnt++;
+    if (exists $seenreads{$acc})
+    {
+      $dupread++;
+    }
+    else
+    {
+      $seenreads{$acc} = 1;
+      $redcnt++;
 
-    print FRG "{FRG\n";
-    foreach my $l (@vals) { print FRG "$l\n"; }
-    print FRG "}\n";
+      my $insrc = 0;
+
+      print FRG "{FRG\n";
+      foreach my $l (@vals) 
+      { 
+        if ($l =~ /^src:/)
+        {
+          #Strip out the src field
+          $insrc = 1;
+        }
+        elsif ($insrc)
+        {
+          if ($l =~ /^\./)
+          {
+            $insrc = 0;
+          }
+        }
+        else
+        {
+          print FRG "$l\n"; 
+        }
+      }
+      print FRG "}\n";
+    }
   }
   else 
   {
@@ -90,3 +156,5 @@ while (<>)
 printTCov($ctgid, 1);
 
 system("rm -f $pid.frg $pid.asm");
+
+print STDERR "reporter:counter:tcov,dupread,$dupread\n";
