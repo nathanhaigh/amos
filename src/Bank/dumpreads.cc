60a7b8053772e575338c85574c0de632872dba93
mcschatz <>
2011-04-14 22:25:45 +0000

    dump reads in fastq format

--- src/Bank/dumpreads.cc
+++ src/Bank/dumpreads.cc
@@ -20,7 +20,9 @@ bool   OPT_BankSpy = false;          // read or read-only spy
 bool   OPT_UseEIDs = false;          // print EIDs instead of IIDs
 bool   OPT_UseRaw  = false;          // pull entire seqlen
 bool   OPT_DumpQual = false;         // dump qualities
-bool   OPT_ShowClear = false;       // show clear range info
+bool   OPT_ShowClear = false;        // show clear range info
+bool   OPT_DumpFastq = false;        // do fastq instead of seq/quals
+char   OPT_FastqOffset = 33;         // Sanger Qualities by default
 
 string OPT_IIDFile;  // Filename of IIDs to dump
 string OPT_EIDFile;  // Filename of EIDs to dump
@@ -103,25 +105,50 @@ void dumpRead(Read_t & red)
 	if(OPT_minIID && red.getIID()<=OPT_minIID || OPT_maxIID && red.getIID()>OPT_maxIID) return;
   } 
 
-  if ( OPT_UseEIDs ) { cout << ">" << red.getEID(); }
-  else               { cout << ">" << red.getIID(); }
-
-  if (OPT_ShowClear)
+  if (OPT_DumpFastq)
   {
-    cout << " 0 0 0 " << red.getClearRange().getLo() + 1 << " " << red.getClearRange().getHi();
-  }
+    if ( OPT_UseEIDs ) { cout << "@" << red.getEID() << endl; }
+    else               { cout << "@" << red.getIID() << endl; }
 
-  cout << endl;
+    if (OPT_UseRaw) { cout << red.getSeqString() << endl; }
+    else            { cout << red.getSeqString(red.getClearRange()) << endl; }
 
-  if (OPT_DumpQual)
-  {
-    if (OPT_UseRaw) { DumpQuals(cout, red.getQualString(), OPT_basesperline); }
-    else            { DumpQuals(cout, red.getQualString(red.getClearRange()), OPT_basesperline); }
+    cout << "+" << endl;
+
+    string quals;
+
+    if (OPT_UseRaw) { quals = red.getSeqString(); }
+    else            { quals = red.getSeqString(red.getClearRange()); }
+
+    for (int i = 0; i < quals.length(); i++)
+    {
+      quals[i] = quals[i] - AMOS::MIN_QUALITY + OPT_FastqOffset;
+    }
+
+    cout << quals << endl;
   }
   else
   {
-    if (OPT_UseRaw) { WrapString (cout, red . getSeqString( ), OPT_basesperline); }
-    else            { WrapString (cout, red . getSeqString(red . getClearRange( )), OPT_basesperline); }
+    if ( OPT_UseEIDs ) { cout << ">" << red.getEID(); }
+    else               { cout << ">" << red.getIID(); }
+
+    if (OPT_ShowClear)
+    {
+      cout << " 0 0 0 " << red.getClearRange().getLo() + 1 << " " << red.getClearRange().getHi();
+    }
+
+    cout << endl;
+
+    if (OPT_DumpQual)
+    {
+      if (OPT_UseRaw) { DumpQuals(cout, red.getQualString(), OPT_basesperline); }
+      else            { DumpQuals(cout, red.getQualString(red.getClearRange()), OPT_basesperline); }
+    }
+    else
+    {
+      if (OPT_UseRaw) { WrapString (cout, red . getSeqString( ), OPT_basesperline); }
+      else            { WrapString (cout, red . getSeqString(red . getClearRange( )), OPT_basesperline); }
+    }
   }
 
   cntw++;
@@ -165,6 +192,7 @@ int main (int argc, char ** argv)
       {
         red_bank.seekg(red_bank.getIDMap().lookupBID(id));
         red_bank >> red;
+
         dumpRead(red);
       }
     }
@@ -219,7 +247,7 @@ void ParseArgs (int argc, char ** argv)
   int ch, errflg = 0;
   optarg = NULL;
 
-  while ( !errflg && ((ch = getopt (argc, argv, "cehrsvqE:I:L:m:M:")) != EOF) )
+  while ( !errflg && ((ch = getopt (argc, argv, "cehrsvqE:I:L:m:M:fQ:")) != EOF) )
     switch (ch)
       {
         case 'c': OPT_ShowClear = true; break;
@@ -232,8 +260,11 @@ void ParseArgs (int argc, char ** argv)
         case 'q': OPT_DumpQual = true; OPT_basesperline = 17; break;
         case 'L': OPT_basesperline = atoi(optarg); break;
 
-	case 'm': OPT_minIID = atoi(optarg); break;
-	case 'M': OPT_maxIID = atoi(optarg); break;        
+	    case 'm': OPT_minIID = atoi(optarg); break;
+	    case 'M': OPT_maxIID = atoi(optarg); break;        
+
+        case 'f': OPT_DumpFastq = true; break;
+        case 'Q': OPT_FastqOffset = atoi(optarg); break;
 
         case 'h':
           PrintHelp (argv[0]);
@@ -266,20 +297,22 @@ void PrintHelp (const char * s)
 
     cerr << "\n.DESCRIPTION.\n"
          << "  Takes an AMOS bank directory and dumps selected reads to\n"
-         << "  stdout in FASTA format.\n"
+         << "  stdout in FASTA or FASTQ format.\n"
          << "\n.OPTIONS.\n"
-         << "  -e            Use EIDs for FastA header instead of IIDs\n"
-         << "  -h            Display help information\n"
-         << "  -r            Ignore clear range and dump entire sequence\n"
+         << "  -f            Dump reads in fastq format\n"
+         << "  -Q int        Use this as the min base quality (default: 33 / Sanger FASTQ)\n"
          << "  -q            Dump qualities in fasta format instead of sequence\n"
+         << "  -e            Use EIDs for FastA header instead of IIDs\n"
+         << "  -r            Ignore clear range and dump entire sequence\n"
          << "  -c            Display clear range information on FASTA header for TIGR Assembler\n"
-         << "  -s            Disregard bank locks and write permissions (spy mode)\n"
-         << "  -v            Display the compatible bank version\n"
          << "  -E file       Dump just the eids listed in file\n"
          << "  -I file       Dump just the iids listed in file\n"
          << "  -L num        Set the maximum number of bases per line (Default: 70)\n"
          << "  -m num        Minimum IID to display (not included) (Default all; not compatible with -e,-E,-I options)\n"
-  	 << "  -M num        Maxim IID to display (included) (Default all; not compatible with -e,-E,-I options)\n"
+  	     << "  -M num        Maximum IID to display (included) (Default all; not compatible with -e,-E,-I options)\n"
+         << "  -h            Display help information\n"
+         << "  -s            Disregard bank locks and write permissions (spy mode)\n"
+         << "  -v            Display the compatible bank version\n"
          << "\n.KEYWORDS.\n"
          << "  amos bank, reads, converters"
          << endl;
@@ -294,6 +327,6 @@ void PrintHelp (const char * s)
 //------------------------------------------------------------ PrintUsage ----//
 void PrintUsage (const char * s)
 {
-  cerr << "\n.USAGE.\n" << "  " <<  s << "  [options]  -b <bank path>\n";
+  cerr << "\n.USAGE.\n" << "  " <<  s << "  [options] <bank path>\n";
   return;
 }
