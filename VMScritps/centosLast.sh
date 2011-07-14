a62a20266969970a8fc5582264b5e59b7a589247
bryanta13 <>
2011-07-14 16:45:21 +0000

    *** empty log message ***

---
+++ VMScritps/centosLast.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+#echo "empty shell !"
+cd /
+cd home/bryanta/
+cd AMOS/
+
+./bootstrap > /home/bryanta/centOSLast.log
+if [ $? -ne 0 ]
+then
+cp /home/bryanta/centOSLast.log /home/bryanta/centOSLast_Failed.log
+/usr/bin/expect <<EOD
+spawn scp /home/bryanta/centOSLast_Failed.log ssh@sauron.cs.umd.edu:VMlogs
+expect "ssh@sauron.cs.umd.edu's password:"
+send "123\r"
+expect eof
+EOD
+echo "1234561" | sudo -S /sbin/shutdown -h now
+fi
+
+./configure --prefix=/usr/local/AMOS >> /home/bryanta/centOSLast.log
+if [ $? -ne 0 ]
+then
+cp /home/bryanta/centOSLast.log /home/bryanta/centOSLast_Failed.log
+/usr/bin/expect <<EOD
+spawn scp /home/bryanta/centOSLast_Failed.log ssh@sauron.cs.umd.edu:VMlogs
+expect "ssh@sauron.cs.umd.edu's password:"
+send "123\r"
+expect eof
+EOD
+echo "1234561" | sudo -S /sbin/shutdown -h now
+fi
+
+make >> /home/bryanta/centOSLast.log
+if [ $? -ne 0 ]
+then
+cp /home/bryanta/centOSLast.log /home/bryanta/centOSLast_Failed.log
+/usr/bin/expect <<EOD
+spawn scp /home/bryanta/centOSLast_Failed.log ssh@sauron.cs.umd.edu:VMlogs
+expect "ssh@sauron.cs.umd.edu's password:"
+send "123\r"
+expect eof
+EOD
+echo "1234561" | sudo -S /sbin/shutdown -h now
+fi
+
+make install >> /home/bryanta/centOSLast.log
+if [ $? -ne 0 ]
+then
+cp /home/bryanta/centOSLast.log /home/bryanta/centOSLast_Failed.log
+/usr/bin/expect <<EOD
+spawn scp /home/bryanta/centOSLast_Failed.log ssh@sauron.cs.umd.edu:VMlogs
+expect "ssh@sauron.cs.umd.edu's password:"
+send "123\r"
+expect eof
+EOD
+echo "1234561" | sudo -S /sbin/shutdown -h now
+fi
+now=$(date +"%y%m%d")
+echo "SUCCESS: complete log stored on http://sauron.cs.umd.edu/$now" >> /home/bryanta/centOSLast.log
+/usr/bin/expect <<EOD
+spawn scp /home/bryanta/centOSLast.log ssh@sauron.cs.umd.edu:VMlogs
+expect "ssh@sauron.cs.umd.edu's password:"
+send "123\r"
+expect eof
+EOD
+rm /home/bryanta/centOSLast.log
+echo "1234561" | sudo -S /sbin/shutdown -h now
---
+++ VMScritps/winXPFirst.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+cd c:
+cd cygwin/
+cd AMOS/
+./bootstrap > /cygdrive/c/cygwin/winXPFirst.log
+if [ $? -ne 0 ]
+then
+cp /cygdrive/c/cygwin/winXPFirst.log /cygdrive/c/cygwin/winXPFirst_Failed.log
+/usr/bin/expect <<EOD
+spawn scp /cygdrive/c/cygwin/winXPFirst_Failed.log ssh@sauron.cs.umd.edu:VMlogs
+expect "ssh@sauron.cs.umd.edu's password:"
+send "123\r"
+expect eof
+EOD
+shutdown /s
+fi
+
+./configure --prefix=/usr/local/AMOS >> /cygdrive/c/cygwin/winXPFirst.log
+if [ $? -ne 0 ]
+then
+cp /cygdrive/c/cygwin/winXPFirst.log /cygdrive/c/cygwin/winXPFirst_Failed.log
+/usr/bin/expect <<EOD
+spawn scp /cygdrive/c/cygwin/winXPFirst_Failed.log ssh@sauron.cs.umd.edu:VMlogs
+expect "ssh@sauron.cs.umd.edu's password:"
+send "123\r"
+expect eof
+EOD
+shutdown /s
+fi
+
+make >> /cygdrive/c/cygwin/winXPFirst.log 
+if [ $? -ne 0 ]
+then
+cp /cygdrive/c/cygwin/winXPFirst.log /cygdrive/c/cygwin/winXPFirst_Failed.log
+/usr/bin/expect <<EOD
+spawn scp /cygdrive/c/cygwin/winXPFirst_Failed.log ssh@sauron.cs.umd.edu:VMlogs
+expect "ssh@sauron.cs.umd.edu's password:"
+send "123\r"
+expect eof
+EOD
+shutdown /s
+fi
+
+make install >> /cygdrive/c/cygwin/winXPFirst.log
+if [ $? -ne 0 ]
+then
+cp /cygdrive/c/cygwin/winXPFirst.log /cygdrive/c/cygwin/winXPFirst_Failed.log
+/usr/bin/expect <<EOD
+spawn scp /cygdrive/c/cygwin/winXPFirst_Failed.log ssh@sauron.cs.umd.edu:VMlogs
+expect "ssh@sauron.cs.umd.edu's password:"
+send "123\r"
+expect eof
+EOD
+shutdown /s
+fi
+now=$(date +"%y%m%d")
+echo "SUCCESS: complete log stored on http://sauron.cs.umd.edu/$now" >> /cygdrive/c/cygwin/winXPFirst.log
+/usr/bin/expect <<EOD
+spawn scp /cygdrive/c/cygwin/winXPFirst.log ssh@sauron.cs.umd.edu:VMlogs
+expect "ssh@sauron.cs.umd.edu's password:"
+send "123\r"
+expect eof
+EOD
+rm /cygdrive/c/cygwin/winXPFirst.log
+shutdown /s
+
