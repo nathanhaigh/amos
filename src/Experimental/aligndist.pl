51f6c80eb102a4e79655ff0c107ca7dd5491413a
Michael Schatz <mschatz@cshl.edu>
2011-07-29 16:17:14 -0400

    rename the reads on the fly

--- src/Experimental/aligndist.pl
+++ src/Experimental/aligndist.pl
@@ -4,9 +4,10 @@ use Statistics::Descriptive;
 use Getopt::Long;
 
 ## Paths to binaries
-my $FASTX_RC   = "fastx_reverse_complement";
-my $FASTX_TRIM = "fastx_trimmer";
-my $BWA        = "bwa";
+my $FASTX_RC     = "fastx_reverse_complement";
+my $FASTX_TRIM   = "fastx_trimmer";
+my $FASTQ_RENAME = "fastq_rename";
+my $BWA          = "bwa";
 
 ## Options
 my $samfile;
@@ -14,26 +15,30 @@ my $showdetails  = 0;
 my $showdist = 0;
 my $help  = 0;
 my $numreads = 10000;
-my $rc = 0;
+my $RC = 0;
 my $trimmean = ".1";  ## trim 10% outliers
 my $QV_TRIM = 10;
 my $HARD_TRIM = 0;
+my $QV_ILLUMINA = 0;
+my $ADD_SUFFIX = 0;
 
 my $prefix;
 
-my $USAGE = "pairDist.pl <options> prefix ref.fa fq1 fq2 | -samfile <samfile>\n"; 
+my $USAGE = "aligndist <options> prefix ref.fa fq1 fq2 | -samfile <samfile>\n"; 
 
 my $res = GetOptions("help"      => \$help,
                      "sam=s"     => \$samfile,
                      "reads=n"   => \$numreads,
                      "dist=n"    => \$showdist,
                      "details=n" => \$showdetails,
-                     "rc"        => \$rc,
+                     "rc"        => \$RC,
+                     "I"         => \$QV_ILLUMINA,
+                     "suffix"    => \$ADD_SUFFIX,
                      "qv=n"      => \$QV_TRIM,
                      "trim=n"    => \$HARD_TRIM,
                      );
  
-if ($help)
+if ($help || !$res)
 {
   print $USAGE;
   print "\n";
@@ -42,18 +47,21 @@ if ($help)
   print "  and compute summary statistics on the unambiguous alignments to the same\n";
   print "  contig/scaffold/chromosome. Use -samfile if reads are already aligned.\n";
   print "\n";
-  print "Options\n";
+  print "Required\n";
   print "  prefix          : prefix for output files\n";
   print "  ref.fa          : path to reference genome (must be indexed with bwa)\n";
   print "  fq1             : path to first read\n";
   print "  fq2             : path to second read\n";
+  print "\n";
+  print "Options\n";
+  print "  -suffix         : Add /1 and /2 suffix to reads\n";
   print "  -reads <n>      : align first n reads (default: $numreads)\n";
   print "  -dist <n>       : show the distance for the first n pairs (default: $showdist)\n";
   print "  -details <n>    : show details for first n pairs (default: $showdetails)\n";
   print "  -rc             : reverse complement the reads before alignment\n";
   print "  -qv <n>         : bwa quality soft quality trim (default: $QV_TRIM)\n";
+  print "  -I              : quality values are Illumina format\n";
   print "  -trim <n>       : hard trim 3' value (default: $HARD_TRIM)\n";
-  print "\n";
   print "  -samfile <file> : Analyze previously aligned reads\n";
 
   exit 0;
@@ -64,6 +72,8 @@ if (($showdetails > 0) && ($showdist > 0))
   die "ERROR: Can't show details and distances at the same time\n";
 }
 
+$QV_ILLUMINA = ($QV_ILLUMINA) ? "-I" : "";
+
 ## Align Reads
 ###############################################################################
 
@@ -72,36 +82,45 @@ if (!defined $samfile)
   die $USAGE if (scalar @ARGV != 4);
 
   $prefix = $ARGV[0];
-  my $ref    = $ARGV[1];
-  my $fq1    = $ARGV[2];
-  my $fq2    = $ARGV[3];
+  my $ref = $ARGV[1];
+  my $fq1 = $ARGV[2];
+  my $fq2 = $ARGV[3];
 
   $samfile = "$prefix.sam.s";
 
   my $nl = $numreads * 4; ## 4 lines per reads in a fastq
 
   my $TRIM_CMD = "";
+  $TRIM_CMD  = " | $FASTX_TRIM -t $HARD_TRIM" if ($HARD_TRIM > 0);
 
-  if ($HARD_TRIM > 0)
+  if ($RC)
   {
-    $TRIM_CMD = " | $FASTX_TRIM -t $HARD_TRIM";
+    $TRIM_CMD .= " | $FASTX_RC -Q ";
+    if ($QV_ILLUMINA) { $TRIM_CMD .= "64"; } else { $TRIM_CMD .= "33"; }
   }
 
-  if ($rc)
+  my $TC1 = $TRIM_CMD;
+  my $TC2 = $TRIM_CMD;
+
+  if ($ADD_SUFFIX)
   {
-    runCmd("prepare fq1",   "$prefix.1.fq",  "head -$nl $fq1 $TRIM_CMD | $FASTX_RC | tr '/' '_' > $prefix.1.fq");
-    runCmd("prepare fq2",   "$prefix.2.fq",  "head -$nl $fq2 $TRIM_CMD | $FASTX_RC | tr '/' '_' > $prefix.2.fq");
+    $TC1 .= " | $FASTQ_RENAME -suffix _1";
+    $TC2 .= " | $FASTQ_RENAME -suffix _2";
   }
   else
   {
-    runCmd("prepare fq1",   "$prefix.1.fq",  "head -$nl $fq1 $TRIM_CMD | tr '/' '_' > $prefix.1.fq");
-    runCmd("prepare fq2",   "$prefix.2.fq",  "head -$nl $fq2 $TRIM_CMD | tr '/' '_' > $prefix.2.fq");
+    $TC1 .= " | $FASTQ_RENAME -tr '/'";
+    $TC2 .= " | $FASTQ_RENAME -tr '/'";
   }
 
-  runCmd("cat fq",     "$prefix.fq",    "cat $prefix.1.fq $prefix.2.fq > $prefix.fq");
-  runCmd("bwa aln",    "$prefix.sai",   "$BWA aln -I -q $QV_TRIM $ref $prefix.fq > $prefix.sai");
-  runCmd("bwa samse",  "$prefix.sam",   "$BWA samse -f $prefix.sam $ref $prefix.sai $prefix.fq");
-  runCmd("sort sam",   "$samfile",      "grep -v '^\@' $prefix.sam | sort > $samfile");
+  runCmd("prepare fq1",   "$prefix.1.fq",  "head -$nl $fq1 $TC1 > $prefix.1.fq", 1);
+  runCmd("prepare fq2",   "$prefix.2.fq",  "head -$nl $fq2 $TC2 > $prefix.2.fq", 1);
+
+  runCmd("cat fq",     "$prefix.fq",    "cat $prefix.1.fq $prefix.2.fq > $prefix.fq", 1);
+  runCmd("bwa index",  "$ref.sa",       "$BWA index $ref");
+  runCmd("bwa aln",    "$prefix.sai",   "$BWA aln $QV_ILLUMINA -q $QV_TRIM $ref $prefix.fq > $prefix.sai", 1);
+  runCmd("bwa samse",  "$prefix.sam",   "$BWA samse -f $prefix.sam $ref $prefix.sai $prefix.fq", 1);
+  runCmd("sort sam",   "$samfile",      "grep -v '^\@' $prefix.sam | sort > $samfile", 1);
 
   print STDERR "SAM file is: $samfile\n";
 }
@@ -195,6 +214,8 @@ foreach my $base (keys %match)
 ###############################################################################
 
 my $num    = $stats->count();
+die "No pairs aligned\n" if ($num == 0);
+
 my $min    = $stats->min();
 my $max    = $stats->max();
 my $mean   = sprintf("%0.01f", $stats->mean());
@@ -216,8 +237,9 @@ sub runCmd
   my $desc = $_[0];
   my $outf = $_[1];
   my $cmd  = $_[2];
+  my $force = $_[3];
 
-  if (! -r $outf)
+  if (! -r $outf || defined $force)
   {
     print STDERR "$desc [$cmd]...\n";
     my $rc = system($cmd);
