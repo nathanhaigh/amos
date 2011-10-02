aae1720be2099933f27a78aa712b6a978370acde
Michael Schatz <mschatz@cshl.edu>
2011-10-02 00:21:18 -0400

    load the coords file so that negative gaps can be handled correctly

--- src/Experimental/patchassembly.pl
+++ src/Experimental/patchassembly.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl -w
 use strict;
 
-my $USAGE = "patchassembly prefix regions ref.fa patches.fa\n";
+my $USAGE = "patchassembly prefix regions ref.fa patches.fa coords\n";
 my $EXTRACT_FASTA = "/bluearc/data/schatz/mschatz/devel/amos/bin/extractFasta";
 my $FASTA_FORMAT = "/data/schatz/software/bin/fasta_formatter";
 
@@ -9,6 +9,7 @@ my $prefix  = shift @ARGV or die $USAGE;
 my $regions = shift @ARGV or die $USAGE;
 my $reffa   = shift @ARGV or die $USAGE;
 my $patches = shift @ARGV or die $USAGE;
+my $coords  = shift @ARGV or die $USAGE;
 
 
 ## Patch management
@@ -49,12 +50,71 @@ sub endPatch
 }
 
 
+## Load coords data
+#####################################################################
+
+my %coords;
+
+my $coordcnt = 0;
+
+print STDERR "Loading alignment coordinates from $coords... ";
+
+open COORDS, "< $coords"
+  or die "Can't open $coords ($!)\n";
+
+#   36948    37958  |        1     1011  |     1011     1011  |   100.00  | 25479815     1011  |     0.00   100.00  |   scaffold_0 scaffold7781.4
+
+while (<COORDS>)
+{
+  $coordcnt++;
+
+  chomp;
+  s/^\s+//;
+
+  my @vals = split /\s+/, $_;
+
+  my $c;
+  $c->{rstart} = $vals[0];
+  $c->{rend}   = $vals[1];
+  $c->{qstart} = $vals[3];
+  $c->{qend}   = $vals[4];
+  $c->{details} = $_;
+  
+  my $ref   = $vals[17];
+  my $patch = $vals[18];
+
+  push @{$coords{$ref}->{$patch}}, $c;
+
+ # print STDERR "$ref\t$patch\t$c->{rstart}\t$c->{rend}\t$c->{qstart}\t$c->{qend}\t||$_\n";   
+
+ #  die if $coordcnt > 10;
+}
+
+close COORDS;
+
+print STDERR "loaded $coordcnt coordinates\n";
+
+
 ## Load reference lengths
 #####################################################################
 my %lengths;
 
-open LENS, "getlengths $reffa |"
-  or die "Can't open pipe to getlengths ($!)\n";
+my $lencnt = 0;
+
+if (-r "$reffa.lens")
+{
+  print STDERR "Loading reference lengths from $reffa.lens... ";
+
+  open LENS, "$reffa.lens"
+    or die "Can't open $reffa.lens ($!)\n";
+}
+else
+{
+  print STDERR "Loading reference lengths from $reffa... ";
+
+  open LENS, "getlengths $reffa |"
+    or die "Can't open pipe to getlengths ($!)\n";
+}
 
 while (<LENS>)
 {
@@ -62,9 +122,14 @@ while (<LENS>)
 
   my @vals = split /\s+/, $_;
   $lengths{$vals[0]} = $vals[1];
+  $lencnt++;
 }
 
 close LENS;
+
+print STDERR "loaded $lencnt reference sequence lengths\n";
+
+
 
 
 ## scan the region files
@@ -75,7 +140,7 @@ close LENS;
 open REGIONS, "< $regions" or die "Can't open $regions ($!)\n";
 
 my $lastseq = "";
-my $lastpos = 1;
+my $lastpos = 0;
 
 while (<REGIONS>)
 {
@@ -102,23 +167,64 @@ while (<REGIONS>)
 
     startPatch($refseq);
     $lastseq = $refseq;
-    $lastpos = 1;
+    $lastpos = 0;
   }
 
-  extractRegion($lastseq, $lastpos, $rstart+1, "+", $reffa);
 
   if ($pend < $pstart)
   {
-    ## this should be the patch itself, but not enough info in the region file ==> punt
-    extractRegion($lastseq, $rstart, $rend+1, "+", $reffa);
+    print STDERR "Negative gap: $_\n";
+
+    my $coordref = $coords{$refseq}->{$patch};
+
+    foreach my $c (@$coordref)
+    {
+       print STDERR "$refseq\t$patch\t$c->{rstart}\t$c->{rend}\t$c->{qstart}\t$c->{qend}\t||$_\n";   
+    }
+
+    if (scalar @$coordref != 2)
+    {
+      print STDERR "complex negative gap! puntting!!!\n";
+
+      extractRegion($lastseq, $lastpos, $rend, "+", $reffa);
+      $lastpos = $rend;
+    }
+    else
+    {
+      my $c1 = $coordref->[0];
+      my $c2 = $coordref->[1];
+
+      my $rs = $c1->{rstart};
+      my $re = $c2->{rend};
+
+      my $qs = $c1->{qstart};
+      my $qe = $c2->{qend};
+      my $qoo = "+";
+
+      if ($qs > $qe)
+      {
+        my $t = $qs; $qs = $qe; $qe = $t;
+        $qoo = "-";
+      }
+
+      die if ($poo && ($qoo eq "+"));
+      die if (!$poo && ($qoo eq "-"));
+
+      print STDERR "replacing  $refseq:[$rs - $re] with $patch:[$qs - $qe]:$qoo\n";
+
+      extractRegion($lastseq, $lastpos, $rs, "+", $reffa);
+      extractRegion($patch, $qs-1, $qe, $qoo, $patches);
+
+      $lastpos = $re;
+    }
   }
   else
   {
-    if ($poo) { extractRegion($patch, $pstart, $pend+1, "-", $patches); }
-    else      { extractRegion($patch, $pstart, $pend+1, "+", $patches); }
+    extractRegion($lastseq, $lastpos, $rstart, "+", $reffa);
+    if ($poo) { extractRegion($patch, $pstart-1, $pend, "-", $patches); }
+    else      { extractRegion($patch, $pstart-1, $pend, "+", $patches); }
+    $lastpos = $rend;
   }
-
-  $lastpos = $rend;
 }
 
 
