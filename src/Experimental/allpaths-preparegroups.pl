25f6f84947a96f669dc5a2cdddeef083ff1a140e
Michael Schatz <mschatz@cshl.edu>
2012-05-06 20:37:07 -0400

    add

--- src/Experimental/allpaths-preparegroups.pl
+++ src/Experimental/allpaths-preparegroups.pl
@@ -17,12 +17,26 @@ while (<>)
   my $set = $dirs[-2];
   my $lib = (split/\./, $set)[0];
 
-  next if /D2/;
-  $path =~ s/D1/D\?/g;
+  my $doprint = 0;
+  my $skipmate = 0;
 
-  $libs{$lib}++;
-  my $libid = $libs{$lib};
+  if    (/_R1_/)   { $doprint = 1; $path =~ s/_R1_/_R\?_/g; }
+  elsif (/_R2_/)   { $skipmate = 1; }
+  elsif (/_RD1\./) { $doprint = 1; $path =~ s/_RD1/_RD\?/g; }
+  elsif (/_RD2\./) { $skipmate = 1; }
+  elsif (/_READ1/) { $doprint = 1; $path =~ s/_READ1/_READ\?/g; }
+  elsif (/_READ2/) { $skipmate = 1; }
+  else
+  {
+    die "Unknown pairing $_\n";
+  }
 
-  print "$lib.$libid, $lib, $path\n";
 
+  if ($doprint)
+  {
+    $libs{$lib}++;
+    my $libid = $libs{$lib};
+
+    print "$lib.$libid, $lib, $path\n";
+  }
 }
